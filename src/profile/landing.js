a249d19438b2a6c710738b76fe661851ba061ade
ChukwukaEze <63220512+ChukwukaEze@users.noreply.github.com>
2023-11-14 01:33:40 +0100

    App Update

--- src/profile/landing.js
+++ src/profile/landing.js
@@ -14,7 +14,7 @@ function Profile() {
     return (
       condition && (
         <p style={{ lineHeight: "2rem" }}>
-          No Projects Found At this Minute. <br /> Chukwuka may be working on a
+          No Projects Found At this Minute! <br /> Chukwuka may be working on a
           project similar to your query, as there are other projects not listed
           here. Confirm from him on WhatsApp.{" "}
           <a rel="noreferrer" href="https://wa.link/7j9t00" target="_blank">
