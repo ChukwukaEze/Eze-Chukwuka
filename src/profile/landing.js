08139d1a34154ab3d1c59b8fa71d1123010f018e
ChukwukaEze <63220512+ChukwukaEze@users.noreply.github.com>
2023-12-01 01:06:32 +0100

    Added Postman Project and Updated At

--- src/profile/landing.js
+++ src/profile/landing.js
@@ -1,29 +1,21 @@
 import React from "react";
-import { FaGoogle, FaWhatsapp } from "react-icons/fa";
-import { BsArrowRight, BsStarFill, BsThreeDots } from "react-icons/bs";
+import { BsStarFill, BsThreeDots } from "react-icons/bs";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import Search from "../components/search";
 import Header from "../components/header";
 import { ProjectBlock, Stacks } from "../components/cards";
 import { SkillsType } from "./projectslist";
+import GitInfo from "react-git-info/macro";
+import ContactCard from "./components/contactCard";
+import { NotFound } from "./components/notFound";
+import moment from "moment";
 
 function Profile() {
-  const { myStacks, projects, SearchField } = Search();
+  const { myStacks, projects, SearchField, filterValues } = Search();
 
-  const NotFound = (condition) => {
-    return (
-      condition && (
-        <p style={{ lineHeight: "2rem" }}>
-          No Projects Found At this Minute! <br /> Chukwuka may be working on a
-          project similar to your query, as there are other projects not listed
-          here. Confirm from him on WhatsApp.{" "}
-          <a rel="noreferrer" href="https://wa.link/7j9t00" target="_blank">
-            Confirm Now!
-          </a>
-        </p>
-      )
-    );
-  };
+  const gitInfo = GitInfo();
+
+  const date = new Date();
 
   return (
     <main className="contact closed">
@@ -33,35 +25,20 @@ function Profile() {
           <div className="main_contact_body">
             <h2 className="headings_contact">Most Rigorous Projects</h2>
             <p>Here are some of my deeply inspiring projects.</p>
+            <br />
+            <p>
+              Last Updated:{" "}
+              {moment(gitInfo.commit.date).format("MMMM Do YYYY, h:mm:ss a")}{" "}
+            </p>
 
             {SearchField()}
-            {NotFound(projects?.length === 0)}
+            {NotFound(projects?.length === 0, filterValues)}
             <div className="projects_rack">
               {projects.map((devProject) => (
                 <ProjectBlock {...devProject} key={devProject?.project} />
               ))}
 
-              <div className="project_item next-project">
-                <p>Chat about your next project</p>
-                <div>
-                  <h3>Let's</h3>
-                  <h3>
-                    Collaborate <BsArrowRight />
-                  </h3>
-                  <div className="contact-bib">
-                    <a rel="noreferrer" href="mailto:ezechukwukae@gmail.com">
-                      <FaGoogle />
-                    </a>
-                    <a
-                      rel="noreferrer"
-                      href="https://wa.link/7j9t00"
-                      target="_blank"
-                    >
-                      <FaWhatsapp />
-                    </a>
-                  </div>
-                </div>
-              </div>
+              <ContactCard />
             </div>
           </div>
           <p style={{ textAlign: "center" }}>
@@ -97,6 +74,12 @@ function Profile() {
             <BsStarFill />
             <BsStarFill />
             <BsStarFill />
+          </div>
+          <div
+            className="star"
+            style={{ textAlign: "center", marginTop: "50px" }}
+          >
+            <small>(c) {date?.getFullYear()}</small>
           </div>
         </div>
       </div>
